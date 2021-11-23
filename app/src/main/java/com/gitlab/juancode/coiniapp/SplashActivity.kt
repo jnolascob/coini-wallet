037398a86a52b67561d1042900ed7915caab7ef9
Juan Carlos Ortiz H <juanortizmg@gmail.com>
2021-11-23 17:16:49 -0500

    Init project and create splash

---
+++ app/src/main/java/com/gitlab/juancode/coiniapp/SplashActivity.kt
@@ -0,0 +1,18 @@
+package com.gitlab.juancode.coiniapp
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.gitlab.juancode.coiniapp.ui.register.RegisterActivity
+
+class SplashActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        Thread.sleep(1000)
+        setTheme(R.style.Theme_CoiniApp)
+
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+
+        startActivity(Intent(this, RegisterActivity::class.java).addFlags(Intent.FLAG_ACTIVITY_NO_ANIMATION))
+    }
+}
---
+++ app/src/main/java/com/gitlab/juancode/coiniapp/ui/register/RegisterActivity.kt
@@ -0,0 +1,12 @@
+package com.gitlab.juancode.coiniapp.ui.register
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.gitlab.juancode.coiniapp.R
+
+class RegisterActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_register)
+    }
+}
